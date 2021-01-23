aae10b6ddb4d668e9d254190fc1e2e899e8e95e6
Ramon MacÃdo <ramon_macedo@live.com>
2021-01-23 01:55:24 -0300

    initial base with antd and less config finished

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Typography } from 'antd';
+
+const { Title } = Typography;
+
+export default function Home() {
+  return (
+    <div>
+      <Title type="secondary">Hello World!</Title>
+    </div>    
+  )
+}
